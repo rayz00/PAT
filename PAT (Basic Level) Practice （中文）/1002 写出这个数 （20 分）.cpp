d4242fe0a178501ec0129ffdf5c58b8f041ac5f3
rayzlol <34232641+rayzlol@users.noreply.github.com>
2018-12-10 16:14:21 +0800

    Update 1002 写出这个数 （20 分）.cpp

--- PAT (Basic Level) Practice （中文）/1002 写出这个数 （20 分）.cpp
+++ PAT (Basic Level) Practice （中文）/1002 写出这个数 （20 分）.cpp
@@ -8,12 +8,36 @@
 输出样例：
 yi san wu
 
-note:PAT的编译器不支持gets()函数
+note:字符串与数字转化，拆解数字
+
+C:
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];	//不可用char *s定义字符串 因为指针没有指向有效的内存可能非法。
+	scanf("%s", s); 
+//	gets(s);	//PAT的C(gcc)编译器不支持gets()函数，但是C(clang)支持甚至自带常用头文件,如stdio.h和string.h
+	const char book[][5] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };	//对照表使用const，可用二维数组（第二个数字表示字符串长度）或指针数组表示字符串列表。
+//	const char *book[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };	
+	int sum = 0;
+	for (int i = 0; i < strlen(s); ++i)
+		sum += s[i] - '0';
+	int t=0,result[10] = { 0 };	//C风格拆解数字的方法
+	while (sum) {
+		result[t++] = sum % 10;
+		sum /= 10;
+	}
+	for (int i = t - 1; i >= 0; --i) {	//倒序输出
+		printf("%s", book[result[i]]);
+		if (i != 0)
+			printf(" ");
+	}
+}
 
 
 
-
-
+C++:
 #include<iostream>
 #include<string>
 using namespace std;
