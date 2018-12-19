65fe3c05863c21ec50bb64aa67dee1b3dee2e054
rayzlol <34232641+rayzlol@users.noreply.github.com>
2018-12-19 17:11:48 +0800

    Update and rename 1002 写出这个数 （20 分）.cpp to 1002 写出这个数 （20 分）.c

--- PAT (Basic Level) Practice （中文）/1002 写出这个数 （20 分）.c
+++ PAT (Basic Level) Practice （中文）/1002 写出这个数 （20 分）.c
@@ -8,20 +8,20 @@
 输出样例：
 yi san wu
 
-//C 循环拆解数字
+//C 	循环拆数字(还有循环组合数字)
 #include<stdio.h>	
 #include<string.h>
 int main()
 {
 	char s[100];	//不可用char *s定义字符串 因为指针没有指向有效的内存可能非法。
 	scanf("%s", s); 
-//	gets(s);	  PAT的C(gcc)编译器不支持gets()函数，但是C(clang)支持甚至自带常用头文件,如stdio.h和string.h
+//	gets(s);	  输入一行。PAT的C(gcc)编译器不支持gets()函数，但是C(clang)支持甚至自带常用头文件,如stdio.h和string.h
 	const char book[][5] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };	//对照表使用const，可用二维数组（第二个数字表示字符串长度）或指针数组表示字符串列表。
 //	const char *book[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };	
 	int sum = 0;
 	for (int i = 0; i < strlen(s); ++i)
 		sum += s[i] - '0';
-	int t=0,result[10] = { 0 };	//C风格拆解数字的方法
+	int t=0,result[10] = { 0 };		//拆数字
 	while (sum) {
 		result[t++] = sum % 10;
 		sum /= 10;
@@ -33,11 +33,10 @@ int main()
 	}
 }
 
-//C++ 转换为字符串类型
+//C++ 	字符串类型
 #include<iostream>	
 #include<string>
 using namespace std;
-
 int main()
 {
 	string s;
@@ -48,8 +47,8 @@ int main()
 		sum += s[i] - '0';
 //	for (auto i : s)	//C++11
 //		sum += i - '0';						
-	string tmp = to_string(sum);	//数字转为字符串
-	for (int i = 0; tmp[i]; ++i) {	//遍历字符串
+	string tmp = to_string(sum);		//字符串
+	for (int i = 0; tmp[i]; ++i) {		//遍历
 		if (i)
 			cout << ' ';
 		cout << book[tmp[i] - '0'];
