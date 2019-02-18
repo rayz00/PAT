1ac9fa5bffe1b6e22e78d4583918d0c4566fe65d
rayzlol <34232641+rayzlol@users.noreply.github.com>
2019-02-18 09:33:50 +0800

    Update 1024 科学计数法 （20 分）.c

--- PAT (Basic Level) Practice （中文）/1024 科学计数法 （20 分）.c
+++ PAT (Basic Level) Practice （中文）/1024 科学计数法 （20 分）.c
@@ -14,3 +14,47 @@
 输出样例 2：
 -12000000000
 
+//分割字符串。我太纠结于用c风格字符串来做，没有想到c++的分割和转换函数
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+	
+int main()
+{	
+	string s;
+	cin >> s;
+	int i = 0;
+	while (s[i] != 'E')
+		i++;
+	string l = s.substr(1, i-1);	//substr左右参数都是闭区间
+	string r = s.substr(i + 1);
+	int n = stoi(r);				      //对应to_string
+	if (s[0] == '-')
+		cout << '-';
+	if (n < 0) {                  //如果指数为负
+		cout << "0.";
+		for (int k = 0; k < abs(n) - 1; ++k)
+			cout << '0';
+		for (int k = 0; l[k]; ++k)
+			if (l[k] != '.')
+				cout << l[k];
+	}
+	else {
+		cout << l[0];               //指数为正
+		if (n >= l.size() - 2) {	  //如果没有小数点
+			for (int j = 2; l[j]; ++j)
+				cout << l[j];
+			for (int j = 0; j < n - l.size() + 2; ++j)
+				cout << 0;
+		}
+		else {
+			int j;	                  //如果还有小数点
+			for (j = 2; j < n + 2; ++j)
+				cout << l[j];		        //打印小数点前
+			cout << '.';
+			for (; l[j]; ++j)		      //打印小数点后
+				cout << l[j];
+		}
+	}
+}	
