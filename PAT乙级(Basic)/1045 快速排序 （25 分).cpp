31ef6dc69907b2b538cc96c27393867d69e42923
rayzlol <rayz0_0@163.com>
2019-03-22 11:15:57 +0800

    -

---
+++ PAT乙级(Basic)/1045 快速排序 （25 分).cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <map>	
+#include <algorithm>
+#include <string>
+#include <list>	
+#include <vector>
+using namespace std;
+
+int n, A[100000], lmax[100000], rmin[100000], cnt = 0;
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> A[i];
+	for (int i = 0, max = i; i < n; ++i)
+		lmax[i] = A[i] >= A[max] ? A[max = i] : A[max];
+	for (int i = n - 1, min = i; i >= 0; --i)
+		rmin[i] = A[i] <= A[min] ? A[min = i] : A[min];
+	for (int i = 0; i < n; ++i) {
+		if (A[i] == lmax[i] && A[i] == rmin[i])
+			cnt++;
+		else
+			A[i] = 0;
+	}
+	cout << cnt << endl;
+	for (int i = 0; i < n && cnt; i++) if (A[i])
+		printf("%d%c", A[i], --cnt ? ' ' : '\0');
+	printf("\n");
+}
---
+++ PAT乙级(Basic)/1046 划拳 （15 分).cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <map>	
+#include <algorithm>
+#include <string>
+#include <list>	
+#include <vector>
+using namespace std;
+
+int main()		
+{	
+	int n, cnta = 0, cntb = 0, a, b, c, d;
+	cin >> n;
+	while (n--) {
+		cin >> a >> b >> c >> d;
+		a += c;
+		if (b == a && d != a)
+			cntb++;
+		else if (d == a && b != a)
+			cnta++;
+	}
+	cout << cnta << ' ' << cntb;
+}	
+
